bcc61132a53f5d569d2ad229f5c2a4e30ccb6421
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-03 10:14:33 -0300

    feat: add form state and get values to parent component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import formInitialValues from './utils/formInitialValues';
 
 function App() {
+  const [formValues, setValues] = useState(formInitialValues);
+  const getFormValues = (values) => {
+    setValues(values);
+  };
+
   return (
     <div className="App">
       <h1>Warfare Unit Card</h1>
-      <Form />
+      <Form getFormValues={ getFormValues } />
+      {
+        console.log(formValues)
+      }
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,40 @@
-export default function Form() {
+import useFormState from "use-form-state/dist/useFormState";
+import formInitialValues from "../utils/formInitialValues";
+
+export default function Form({ getFormValues }) {
+  const { getNativeInputProps, values } = useFormState(formInitialValues);
+
+  const returnValues = () => {
+    const formValues = values;
+    getFormValues(formValues);
+  }
+
   return(
     <div>
       <form>
         <label>
           <span>Unit Name: </span>
-          <input type="text" placeholder="Unit Name" />
+          <input
+            type="text"
+            placeholder="Unit Name"
+            { ...getNativeInputProps('unitName') }
+            required
+          />
         </label>
         <label>
           <span>Commander: </span>
-          <input type="text" placeholder="Commander Name" />
+          <input
+            type="text"
+            placeholder="Commander Name"
+            { ...getNativeInputProps('commanderName') }
+          />
         </label>
         <label>
           <span>Type: </span>
-          <select name="unitType">
+          <select
+            name="unitType"
+            {...getNativeInputProps('unitType') }
+          >
             <option value="infantry">Infantry</option>
             <option value="artillery">Artillery</option>
             <option value="cavalry">Cavalry</option>
@@ -23,11 +45,19 @@ export default function Form() {
         </label>
         <label>
           <span>Ancestry: </span>
-          <input type="text" placeholder="Ancestry" />
+          <input
+            type="text"
+            placeholder="Ancestry"
+            { ...getNativeInputProps('ancestry') }
+            required
+          />
         </label>
         <label>
           <span>Unit Size: </span>
-          <select name="unitSize">
+          <select
+            name="unitSize"
+            { ...getNativeInputProps('unitSize') }
+          >
             <option value="4">4</option>
             <option value="6">6</option>
             <option value="8">8</option>
@@ -37,7 +67,10 @@ export default function Form() {
         </label>
         <label>
           <span>Experience: </span>
-          <select name="unitExperience">
+          <select
+            name="unitExperience"
+            { ...getNativeInputProps('unitExperience') }
+          >
             <option value="levies">Levies</option>
             <option value="regular">Regular</option>
             <option value="veteran">Veteran</option>
@@ -47,7 +80,10 @@ export default function Form() {
         </label>
         <label>
           <span>Equipment: </span>
-          <select name="unitEquipment">
+          <select
+            name="unitEquipment"
+            { ...getNativeInputProps('unitEquipment') }
+          >
             <option value="light">Light</option>
             <option value="medium">Medium</option>
             <option value="heavy">Heavy</option>
@@ -58,44 +94,74 @@ export default function Form() {
           <span>Unit Statistics</span>
           <label>
             <span>ATK: </span>
-            <input type="number" defaultValue="1" />
+            <input
+              type="number"
+              { ...getNativeInputProps('atk') }
+            />
           </label>
           <label>
             <span>DEF: </span>
-            <input type="number" defaultValue="10" />
+            <input
+              type="number"
+              { ...getNativeInputProps('def') }
+            />
           </label>
           <label>
             <span>POW: </span>
-            <input type="number" defaultValue="1" />
+            <input
+              type="number"
+              { ...getNativeInputProps('pow') }
+            />
           </label>
           <label>
             <span>TOU: </span>
-            <input type="number" defaultValue="10" />
+            <input
+              type="number"
+              { ...getNativeInputProps('tou') }
+            />
           </label>
           <label>
             <span>MOR: </span>
-            <input type="number" defaultValue="1" />
+            <input
+              type="number"
+              { ...getNativeInputProps('mor') }
+            />
           </label>
           <label>
             <span>COM: </span>
-            <input type="number" defaultValue="1" />
+            <input
+              type="number"
+              { ...getNativeInputProps('com') }
+            />
           </label>
         </div>
         <label>
           <span>Number of Attacks: </span>
-          <input type="number" defaultValue="1" />
+          <input
+            type="number"
+            { ...getNativeInputProps('numberAtks') }
+          />
         </label>
         <label>
           <span>Damage: </span>
-          <input type="number" defaultValue="1" />
+          <input
+            type="number"
+            { ...getNativeInputProps('dmg') }
+          />
         </label>
         <label>
           <span>Tier: </span>
-          <input type="number" max="5" defaultValue="1" />
+          <input
+            type="number"
+            max="5"
+            { ...getNativeInputProps('tier') }
+          />
         </label>
+        <button
+          type="button"
+          onClick={ returnValues }
+        >Submit</button>
       </form>
     </div>
   );
 }
-
-// infantry, artillery, cavalry, aerial, siege, levy
