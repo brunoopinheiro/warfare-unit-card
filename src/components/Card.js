0ff06d6994b41af1614abbd35c880c07eebd4055
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-04 12:16:19 -0300

    style: basic grid layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 import stringToArray from '../utils/stringToArray';
-import getUnitIcon, { unitCard } from '../assets';
+import getUnitIcon from '../assets';
 
 export default function Card({ formValues }) {
   const {
@@ -32,28 +32,38 @@ export default function Card({ formValues }) {
   return (
     <div className="card-container">
       <div className="unit-card">
-        <p className="unit-name">{unitName}</p>
-        <p className="unit-commander">{commanderName}</p>
-        <img src={ unitIcon } alt="unit type icon" />
-        <div className="big-number-display">{ tier }</div>
-        <div className="big-number-display">{ unitSize }</div>
-        <div className="big-number-display">{ numberAtks }</div>
-        <div className="big-number-display">{ dmg }</div>
-        <div className="big-stat-display">{ `+${atk}` }</div>
-        <div className="big-stat-display">{ def }</div>
-        <div className="big-stat-display">{ `+${pow}` }</div>
-        <div className="big-stat-display">{ tou }</div>
-        <div className="big-stat-display">{ `+${mor}` }</div>
-        <div className="big-stat-display">{ `+${com}` }</div>
-        <span className="tags">{tags}</span>
-        <ul className="traits">
-          {
-            traitsArr.map((trait, index) => (
-              <li key={`${trait}-${index}`}>{trait}</li>
-            ))
-          }
-        </ul>
-        <img src={ unitCard} alt="unit card background" className="card-background" />
+        <div className="left-column">
+          <div className="big-number-display">{ tier }</div>
+          <img src={ unitIcon } alt="unit type icon" className="type-icon" />
+          <div className="big-number-display">{ numberAtks }</div>
+          <div className="big-number-display">{ dmg }</div>
+        </div>
+        <div className="right-column">
+          <div className="top-right-container">
+            <div className="names-tags">
+              <p className="unit-name">{unitName}</p>
+              <p className="unit-commander">{commanderName}</p>
+              <p className="tags">{tags}</p>
+            </div>
+            <div className="big-number-display">{ unitSize }</div>
+          </div>
+          <div className="stats-container">
+            <div className="big-stat-display">{ `+${atk}` }</div>
+            <div className="big-stat-display">{ def }</div>
+            <div className="big-stat-display">{ `+${pow}` }</div>
+            <div className="big-stat-display">{ tou }</div>
+            <div className="big-stat-display">{ `+${mor}` }</div>
+            <div className="big-stat-display">{ `+${com}` }</div>
+          </div>
+          <ul className="traits">
+            {
+              traitsArr.map((trait, index) => (
+                <li key={`${trait}-${index}`}>{trait}</li>
+              ))
+            }
+          </ul>
+        </div>
+        {/* <img src={ unitCard} alt="unit card background" className="card-background" /> */}
       </div>
       <button
         onClick={ () => console.log(document.getElementsByClassName('unit-card')[0]) }
