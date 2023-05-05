822d082fbf6d788c1133f4f865c248be0d921344
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-04 23:11:22 -0300

    style: first steps styling form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import useFormState from "use-form-state/dist/useFormState";
 import formInitialValues from "../utils/formInitialValues";
+import './Form.css';
 
 export default function Form({ getFormValues }) {
   const { getNativeInputProps, values } = useFormState(formInitialValues);
@@ -10,88 +11,92 @@ export default function Form({ getFormValues }) {
   }
 
   return(
-    <div>
+    <div className="form-container">
       <form>
-        <label>
-          <span>Unit Name: </span>
-          <input
-            type="text"
-            placeholder="Unit Name"
-            { ...getNativeInputProps('unitName') }
-            required
-          />
-        </label>
-        <label>
-          <span>Commander: </span>
-          <input
-            type="text"
-            placeholder="Commander Name"
-            { ...getNativeInputProps('commanderName') }
-          />
-        </label>
-        <label>
-          <span>Type: </span>
-          <select
-            name="unitType"
-            {...getNativeInputProps('unitType') }
-          >
-            <option value="infantry">Infantry</option>
-            <option value="artillery">Artillery</option>
-            <option value="cavalry">Cavalry</option>
-            <option value="aerial">Aerial</option>
-            <option value="siege">Siege</option>
-            <option value="levy">Levy</option>
-          </select>
-        </label>
-        <label>
-          <span>Ancestry: </span>
-          <input
-            type="text"
-            placeholder="Ancestry"
-            { ...getNativeInputProps('ancestry') }
-            required
-          />
-        </label>
-        <label>
-          <span>Unit Size: </span>
-          <select
-            name="unitSize"
-            { ...getNativeInputProps('unitSize') }
-          >
-            <option value="4">4</option>
-            <option value="6">6</option>
-            <option value="8">8</option>
-            <option value="10">10</option>
-            <option value="12">12</option>
-          </select>
-        </label>
-        <label>
-          <span>Experience: </span>
-          <select
-            name="unitExperience"
-            { ...getNativeInputProps('unitExperience') }
-          >
-            <option value="levies">Levies</option>
-            <option value="regular">Regular</option>
-            <option value="veteran">Veteran</option>
-            <option value="elite">Elite</option>
-            <option value="superElite">Super Elite</option>
-          </select>
-        </label>
-        <label>
-          <span>Equipment: </span>
-          <select
-            name="unitEquipment"
-            { ...getNativeInputProps('unitEquipment') }
-          >
-            <option value="light">Light</option>
-            <option value="medium">Medium</option>
-            <option value="heavy">Heavy</option>
-            <option value="superHeavy">Super-Heavy</option>
-          </select>
-        </label>
-        <div>
-          <span>Unit Statistics</span>
+        <div className="text-inputs">
+          <label>
+            <input
+              type="text"
+              placeholder="Unit Name"
+              { ...getNativeInputProps('unitName') }
+              required
+            />
+            <span>Unit Name</span>
+          </label>
+          <label>
+            <span>Commander: </span>
+            <input
+              type="text"
+              placeholder="Commander Name"
+              { ...getNativeInputProps('commanderName') }
+            />
+          </label>
+          <label>
+            <span>Ancestry: </span>
+            <input
+              type="text"
+              placeholder="Ancestry"
+              { ...getNativeInputProps('ancestry') }
+              required
+            />
+          </label>
+        </div>
+        <div className="select inputs">
+          <label>
+            <span>Type: </span>
+            <select
+              name="unitType"
+              {...getNativeInputProps('unitType') }
+            >
+              <option value="infantry">Infantry</option>
+              <option value="artillery">Artillery</option>
+              <option value="cavalry">Cavalry</option>
+              <option value="aerial">Aerial</option>
+              <option value="siege">Siege</option>
+              <option value="levy">Levy</option>
+            </select>
+          </label>
+          <label>
+            <span>Unit Size: </span>
+            <select
+              name="unitSize"
+              { ...getNativeInputProps('unitSize') }
+            >
+              <option value="4">4</option>
+              <option value="6">6</option>
+              <option value="8">8</option>
+              <option value="10">10</option>
+              <option value="12">12</option>
+            </select>
+          </label>
+          <label>
+            <span>Experience: </span>
+            <select
+              name="unitExperience"
+              { ...getNativeInputProps('unitExperience') }
+            >
+              <option value="levies">Levies</option>
+              <option value="regular">Regular</option>
+              <option value="veteran">Veteran</option>
+              <option value="elite">Elite</option>
+              <option value="super-elite">Super Elite</option>
+            </select>
+          </label>
+          <label>
+            <span>Equipment: </span>
+            <select
+              name="unitEquipment"
+              { ...getNativeInputProps('unitEquipment') }
+            >
+              <option value="light">Light</option>
+              <option value="medium">Medium</option>
+              <option value="heavy">Heavy</option>
+              <option value="super-heavy">Super-Heavy</option>
+            </select>
+          </label>
+        </div>
+        <fieldset>
+          <legend>Unit Statistics</legend>
           <label>
             <span>ATK: </span>
             <input
@@ -134,42 +139,44 @@ export default function Form({ getFormValues }) {
               { ...getNativeInputProps('com') }
             />
           </label>
+        </fieldset>
+        <div className="dmg-inputs">
+          <label>
+            <span>Number of Attacks: </span>
+            <input
+              type="number"
+              { ...getNativeInputProps('numberAtks') }
+            />
+          </label>
+          <label>
+            <span>Damage: </span>
+            <input
+              type="number"
+              { ...getNativeInputProps('dmg') }
+            />
+          </label>
+          <label>
+            <span>Tier: </span>
+            <input
+              type="number"
+              max="5"
+              { ...getNativeInputProps('tier') }
+            />
+          </label>
         </div>
-        <label>
-          <span>Number of Attacks: </span>
-          <input
-            type="number"
-            { ...getNativeInputProps('numberAtks') }
-          />
-        </label>
-        <label>
-          <span>Damage: </span>
-          <input
-            type="number"
-            { ...getNativeInputProps('dmg') }
-          />
-        </label>
-        <label>
-          <span>Tier: </span>
-          <input
-            type="number"
-            max="5"
-            { ...getNativeInputProps('tier') }
-          />
-        </label>
         <label>
           <span>Traits: </span>
           <textarea
             { ...getNativeInputProps('traits') }
           />
         </label>
-        <button
-          type="button"
-          onClick={ returnValues }
-        >
-          Update Card!
-        </button>
       </form>
+      <button
+        type="button"
+        onClick={ returnValues }
+      >
+        Update Card!
+      </button>
     </div>
   );
 }
