6c64adb7ae2996687ae224d4b0fa05d523a4b460
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-05 10:43:49 -0300

    style: footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
-import { linkedIn, github } from "../utils/links";
+import { linkedIn, github, knw } from "../utils/links";
+import './Footer.css';
 
 export default function Footer() {
   return (
     <footer>
+      <p>This project was made to work with <a href={knw}>Kingdoms & Warfare</a>, by MCDM.</p>
       <p>Let's connect on <a href={linkedIn}>Linkedin</a></p>
       <p>Check my other projects on <a href={github}>Github</a></p>
       {/* <p>Access the <a href={}>repository</a> for this project!</p> */}
--- src/utils/links.js
+++ src/utils/links.js
@@ -1,6 +1,7 @@
 const linkedIn = 'https://www.linkedin.com/in/brunoopinheirojr/';
 const github = 'https://github.com/brunoopinheiro';
+const knw = 'https://shop.mcdmproductions.com/collections/kingdoms-warfare/products/kingdoms-warfare-book-reprint-pdf-preorder';
 // const repo = '';
 // const kofi = '';
 
-export { linkedIn, github };
+export { linkedIn, github, knw };
